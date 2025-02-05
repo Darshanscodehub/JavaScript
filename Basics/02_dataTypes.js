73a0679cf84e5de75d1a9ede7916f3395b6d16a4
Darshan Deshmukh <147388318+Darshanscodehub@users.noreply.github.com>
2025-02-05 09:17:34 +0000

    datatypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version of javaScript
+
+// alert(3+3); //we are using node js not browser
+
+let name = "Darshan"
+let age = 21
+let isLoggedIn = false
+
+//number => 2^53
+//bigint
+//string => ""
+//bollean => true, false
+// null => standalone value
+//undefined => value is not defined
+//symbol => unique
+//object 
+
+console.log(typeof "Darshan")
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+console.log(typeof age)
