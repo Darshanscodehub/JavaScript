16bef7f6ad1a0740bad9674d9d36bc76d5b6be81
Darshan Deshmukh <147388318+Darshanscodehub@users.noreply.github.com>
2025-02-05 10:01:46 +0000

    Type conversion

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,85 @@
+let score = 33
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))//number
+
+let score2 = "33abc"
+// console.log(typeof (score2))//string
+let valueInNumber2= Number(score2)
+// console.log(valueInNumber2)//Nan
+// console.log(typeof (valueInNumber2))//number
+
+let score3 = null
+// console.log(typeof score3)//object
+
+let score4 = undefined
+// console.log(typeof score4);
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1 false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)//true
+
+let isLoggedIn2 = ""
+
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn2)//false
+
+//1=>true; 0=>false
+// "" => false
+//"Darshan" => true
+
+
+
+// ************** Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)//-3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "Darshan"
+let str3 = str1 + str2
+// console.log(str3); HelloDarshan
+
+// console.log("1" + 2) //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+console.log(true); //true
+console.log(+true); //1
+// console.log(true+);//invalid code
+console.log(+false); //0
+console.log(+""); //0
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
