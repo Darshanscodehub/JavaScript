798e1c15ef4c0eedf7752a7384681bb347aec92d
Darshanscodehub <darshandeshmukh1111@gmail.com>
2025-02-11 19:45:06 +0530

    for each loop[

---
+++ 05_iteration/five.js
@@ -0,0 +1,87 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);// Array of programming languages
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// Using forEach to iterate over the array and log each value
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// Using arrow function with forEach to iterate over the array and log each value
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// Defining a function to print the value
+// function printMe(item){
+//     console.log(item);
+// }
+
+// Using the defined function with forEach to iterate over the array and log each value
+// coding.forEach(printMe)
+
+// Using forEach to iterate over the array and log each value, index, and the array itself
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+// Array of objects containing language information
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+// Using forEach to iterate over the array of objects and log the language name
+myCoding.forEach( (item) => {
+    // Logging the language name
+    console.log(item.languageName);
+} )
+} )
